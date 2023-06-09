75a801bbe73bc029e6fdb6f8756eee55509d61cb
Christian Garcia <bones192nd@gmail.com>
2023-06-09 14:22:27 -0400

    fun lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.owner = owner
+        self.__setattr__("pet_type", pet_type)  #you're accessing the pet_type method, this is how the value is validated
+        self.all.append(self)
+
+    # semi baffles my mind. still used to variable name = property(fgetter, fsetter)
+    @property
+    def pet_type(self):   #fgetter
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, ptype):
+        if ptype in self.PET_TYPES:
+            self._pet_type = ptype
+        else:
+            raise Exception
+
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception
+        else:
+            pet.owner = self
+
+
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key = lambda x: x.name )
